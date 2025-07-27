06664f9b71be32ecd47d093f3d0557f3c75f10e0
amir hossein <amohammadi161183@gmail.com>
2025-07-27 17:08:03 +0330

    required and include

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h2 style='color: crimson'>use from modules.php
+    file context in include.php file</h2>";
+    include "modules.php";
+    echo $name . "<br>";
+    echo $age . "<br>";
+?>
---
+++ modules.php
@@ -0,0 +1,8 @@
+<?php
+    echo "<h1>hello world</h1>
+    <p>include (or require) statement takes all 
+    the text/code/markup that exists in the specified 
+    file and copies it into the file that uses the include statement.</p>";
+    $name = "amir hossein";
+    $age = 21;
+?>
